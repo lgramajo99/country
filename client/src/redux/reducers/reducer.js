bb5ef6053698192c5f3f155c9bde72b171694323
lgramajo99 <lucianongramajo@gmail.com>
2023-05-20 00:20:57 -0300

    creacion y configuracion de redux ok

--- client/src/redux/reducers/reducer.js
+++ client/src/redux/reducers/reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import getCountryReducer from './getCountry.reducer';
 
 const rootReducer = combineReducers({
-
+    getCountry: getCountryReducer,
 })
 
 export default rootReducer
