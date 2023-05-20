9203475c7eac54c44f347fc5fd3e9c8b8f1c3c1b
lgramajo99 <lucianongramajo@gmail.com>
2023-05-20 19:25:24 -0300

    componente Landing.jsx listo

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Errorpage from './components/errorpage/Errorpage';
 import Footer from './components/footer/Footer.jsx';
 import Inicio from './components/inicio/Inicio.jsx';
+import Landing from './components/landing/Landing';
 import Nav from './components/nav/Nav.jsx';
 import { Route, Routes } from 'react-router-dom';
 
@@ -10,7 +11,7 @@ function App() {
     <div className="App">
       <Nav />
       <Routes >
-        
+        <Route path='/' element={<Landing />} />
         <Route path={'/inicio'} element={<Inicio />} />
         <Route path={'*'} element={<Errorpage />} />
       </Routes>
---
+++ client/src/components/landing/Landing.jsx
@@ -0,0 +1,40 @@
+import style from './Landing.module.css';
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+
+function Landing() {
+    const [isOpenModal, setIsOpenModal] = useState(false);
+
+    const handleSwitchLogin = () => { setIsOpenModal(!isOpenModal) }
+
+    const navigate = useNavigate()
+    const handlerInvitado = () => { navigate('/inicio') };
+
+
+    return (
+        <section className={style.landing}>
+            {
+                !isOpenModal ?
+                    <article className={style.principal}>
+                        <h2>Encuentra el proximo destino para tus vacaciones.</h2>
+
+                        <p>Busca las mejores actividades que se adapten a ti y a tu familia en todas partes del mundo.</p>
+
+                        <button className={style.boton} onClick={handlerInvitado}>Entrar como invitado</button>
+                        <button className={style.boton} onClick={handleSwitchLogin}>Entrar como administrador</button>
+                    </article>
+                    : //  ######################################################## 
+                    <article>
+                        <form className={style.formulario}>
+                            <input className={style.input} type="text" placeholder='Usuario' />
+                            <input className={style.input} type='password' placeholder='Contraseña' />
+                            <button className={style.boton}> Entrar </button>
+                        </form>
+                        <button className={style.boton} onClick={handleSwitchLogin}>Volver atras</button>
+                    </article>
+            }
+        </section >
+    )
+}
+
+export default Landing;
