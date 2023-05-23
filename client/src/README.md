2e106d5f62972f7882bd599f1f3997f1dad935c6
lgramajo99 <lucianongramajo@gmail.com>
2023-05-23 16:52:53 -0300

    agregamos responsive a la pagina

---
+++ client/src/README.md
@@ -0,0 +1,31 @@
+# Estilos del proyecto Countries
+
+## Tamaños responsive:
+
+### Estilos para pantallas pequeñas.    
+    @media(max-width: 767.98px){}
+
+### Estilos para pantallas medianas.
+    @media(min-width: 768px) and (max-width: 991.98px){}
+
+### Estilos para pantallas grandes.
+    @media(min-width: 992px){}
+
+
+## Colores hexageximal:
+
+```
+1. Rojo: #FF0000
+2. Rosy Brown: #bba6a6
+    - Nav.jsx
+    - Footer.jsx
+    - Side.jsx
+3. Indian Red: #e76a6a
+    - Card.jsx
+4. Light Rose: #e4cccc
+    - Cardlist.jsx
+5. Slate Gray: #706E6E
+    - Landing.jsx
+```
+
+
--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,17 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import style from './Nav.module.css';
 import Searchbar from '../utils/searchbar/Searchbar';
 
 function Nav() {
+    const navigate = useNavigate()
+    const toHome = () => { navigate('/inicio') }
+
     return (<nav className={style.nav}>
-        <h1>Country</h1>
+        <h1 className={style.logo} onClick={toHome}>Country</h1>
         <ul className={style.menu}>
 
             <li><Searchbar /></li>
             <li><Link to={'/inicio'}>Inicio</Link></li>
             <li><Link to={'/paises'}>Paises</Link></li>
             <li><Link to={'/nosotros'}>Sobre nosotros</Link></li>
-            <li><Link to={'/administracion'}>⚙</Link></li>
+            <li><Link to={'/administracion'}> Administracion ⚙</Link></li>
         </ul>
     </nav>)
 }
